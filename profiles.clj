df655edf46c8b04d9cd85a83399e511e059eef17
Lu Liu <luliu27@gmail.com>
2015-06-17 15:33:06 -0400

    add latest nrepl dep; upgrade cider-nrepl version, and add lein-try plugin

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,10 @@
 {:user {:dependencies [[org.clojure/tools.namespace "0.2.10"]
+                       [org.clojure/tools.nrepl "0.2.10"]
                        [potemkin "0.3.13"]
                        [alembic "0.3.2"]
                        [criterium "0.4.3"]]
-        :plugins [[cider/cider-nrepl "0.8.1"]
+        :plugins [[cider/cider-nrepl "0.9.0"]
                   [lein-cljfmt "0.1.7"]
                   [jonase/eastwood "0.2.1"]
-                  [lein-kibit "0.0.8"]]}}
+                  [lein-kibit "0.0.8"]
+                  [lein-try "0.4.3"]]}}
