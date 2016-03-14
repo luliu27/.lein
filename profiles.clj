70f522339358ad204ee19b07a728700c1a1272bc
Lu Liu <luliu27@gmail.com>
2016-03-13 21:45:26 -0400

    add test-refresh and ultra, upgrade kibit

--- profiles.clj
+++ profiles.clj
@@ -7,5 +7,15 @@
                   [refactor-nrepl "1.2.0"]
                   [lein-cljfmt "0.3.0"]
                   [jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.2"]
                   [lein-try "0.4.3"]]}}
+                  [jonase/eastwood "0.2.3"]
+                  [lein-try "0.4.3"]
+                  [venantius/ultra "0.4.1"]
+                  [com.jakemccrary/lein-test-refresh "0.14.0"]
+                  [lein-kibit "0.1.2"]]
+        :test-refresh {:changes-only true
+                       :growl false
+                       :notify-command ["notify"]
+                       :quiet true}
+        :ultra {:color-scheme :solarized_dark
+                :repl false}}}
